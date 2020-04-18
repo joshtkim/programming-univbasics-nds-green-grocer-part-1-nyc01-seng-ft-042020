a61b07c5d916e8a2cda80f18b33506f65841f91e
Joshua Kim <jykim92@gmail.com>
2020-04-18 16:52:48 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -5,9 +5,7 @@ def find_item_by_name_in_collection(name, collection)
   #
   # Consult README for inputs and outputs
   collection.each do |key, item|
-    item.each do |hash|
     if hash == name
-    end
       return item
     binding.pry
     end
