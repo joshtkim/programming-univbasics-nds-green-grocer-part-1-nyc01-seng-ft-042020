f7ecc1fb62f7143a7a22f6f9db40166ca6ee48ea
Joshua Kim <jykim92@gmail.com>
2020-04-18 17:03:15 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -5,8 +5,9 @@ def find_item_by_name_in_collection(name, collection)
   #
   # Consult README for inputs and outputs
   collection.each do |item|
-    until item == name
+    unless item == name
       return nil
+    else
       return name
     end
       binding.pry
