9c135ece64aeeeff1de1814e8dccbdb502b6646f
Joshua Kim <jykim92@gmail.com>
2020-04-18 16:39:57 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,6 +2,11 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  collection.find do |item|
+    if item == name
+      return item
+    end
+    
 
 end
 
